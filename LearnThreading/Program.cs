346443e7a7d42561dae3da54c43e95c8b64e5175
Eldeniz Ismayilov <eldeniz647@gmail.com>
2018-10-20 21:44:27 +0400

    modified program file

--- LearnThreading/Program.cs
+++ LearnThreading/Program.cs
@@ -2,13 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace LearnThreading
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void ThreadMethod()
         {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
         }
+        public static void Main()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+            t.Join();
+
+            Console.ReadKey();
+        }
+
+      
     }
 }
